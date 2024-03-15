ce8352afd50e56e3489a561d7c0b2a5249236dcb
Drew Brown <drewkyle01@gmail.com>
2024-03-15 10:42:56 -0400

    Create Hero and Computer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
-
-
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Hero/>
+          <Hero />
         </div>
         <About />
         <Experience />
@@ -18,11 +27,11 @@ const App = () => {
         <Feedbacks />
         <div className="relative z-0">
           <Contact />
-          <StarsCanvas/>
+          <StarsCanvas />
         </div>
-        </div>
+      </div>
     </BrowserRouter>
   );
-}
+};
 
-export default App
+export default App;
